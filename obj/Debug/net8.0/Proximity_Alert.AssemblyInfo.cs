35818570a2b1e2072bcc2b965cbd1b5ae60ef058
Teodor Mihail <teodormihail07@gmail.com>
2024-07-28 02:01:40 +0100

    * UPDATE

--- obj/Debug/net8.0/Proximity_Alert.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proximity_Alert.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proximity_Alert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d265b6d596c1098dcd070eb5b6e96db94c100d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad451f7d79ef509904ddac13fcfa98ff03f43b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proximity_Alert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proximity_Alert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
